d1a92d4280a922ac450afe8701b8cd265d4a808e
Tiffany <tifchang@wharton.upenn.edu>
2017-07-27 12:05:15 -0700

    incorporated modals but needs styling

---
+++ reactApp/components/CollaborateDocModal.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Modal from 'react-modal';
+import axios from 'axios';
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+class CollaborateDocModal extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      collabId: '',
+      docPassword: '',
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  handleSubmit() {
+    const attemptCollaboration = this.props.attemptCollaboration;
+    attemptCollaboration();
+    this.closeModal();
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.openModal}>Collaborate!</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Become a new collaborator!"
+          >
+
+            <h2 ref={subtitle => this.subtitle = subtitle}>Enter the collab id and password to collaborate</h2>
+
+            <form
+              onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                placeholder="Collaboration Id"
+                value={this.state.collabId}
+                onChange={(e) => this.setState({collabId: e.target.value})}
+              /> <br></br>
+              <input
+                type="password"
+                placeholder="Document Password"
+                value={this.state.docPassword}
+                onChange={(e) => this.setState({docPassword: e.target.value})}
+              /> <br></br>
+              <input
+                type="submit"
+              />
+            </form>
+          </Modal>
+        </div>
+    );
+  }
+}
+
+export default CollaborateDocModal;
